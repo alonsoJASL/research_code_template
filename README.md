02bee410ab6a76d323a90b9b28105cf68c1b0925
Jose Alonso Solis-Lemus <jsolislemus@cemrg-mbp.home>
2023-07-10 13:09:24 +0100

    base_cnonfig

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# research_code_template
+> Edit this file. 
+
+# Research Code Template
 Template for developing research code. Has support to add other code as submodules.
+
+It uses the following code: 
++ [CemrgApp](https://github.com/OpenHeartDevelopers/CemrgApp): 
+    + Branch: `<some_branch>`
+    + Commit: [`commit_sha`]() 
+    + Notes: Some notes on this repo
++ [imatools]()
+    + Branch: `<some_branch>`
+    + Commit: [`commit_sha`](https://github.com/alonsoJASL/imatools.git)
+
+## Quick start
+Clone this repository: 
+```shell
+git clone <this_repository>  
+cd <this_repository>
+```
+
+> `imatools` is a common repository used for image analysis and mesh handling in python
+
+Add `imatools` as submodule:
+```shell
+git submodule add https://github.com/alonsoJASL/imatools.git imatools
+git submodule init
+git submodule update 
+```
---
+++ process.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import argparse 
+
+def chooseplatform():
+    return sys.platform
+
+# path to the current file
+def PROCESS_PATH():
+    return os.path.dirname(os.path.realpath(__file__))
+
+PROCESS_PATH = PROCESS_PATH()
+
+sys.path.append(PROCESS_PATH)
+sys.path.append(os.path.join(PROCESS_PATH, 'imatools'))
+
+try: 
+    from imatools.common import vtktools as vtku 
+    from imatools.common import itktools as itku
+    from imatools.common import ioutils as iou
+
+catch Exception as e:
+    print("Error: ", e)
+
+import numpy as np
+import pandas as pd
+
+def main(args):
+    print(args)
+
+if __name__ == '__main__':
+    in_parser = argparse.ArgumentParser(description='SOME PROCESS.')
+    in_parser.add_argument('-i', '--input', type=str, help='Input file.')
+    in_parser.add_argument('-o', '--output', type=str, help='Output file.')
+
+    args = in_parser.parse_args()
+    main(args)
+
+
